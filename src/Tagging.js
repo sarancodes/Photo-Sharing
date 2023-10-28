227d5bc81ac76cc20265aba99f343a24570533ad
Saran Kumar <sarancodes@gmail.com>
2023-10-28 17:22:50 +0530

    Update Tagging.js

--- src/Tagging.js
+++ src/Tagging.js
@@ -11,9 +11,9 @@ function TagObjectAsHidden() {
       try {
         AWS.config.update({
             
-    accessKeyId: 'AKIATTU73NOEPC34P4LO',
-    secretAccessKey: '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',
-    region: 'us-east-1',
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: '',
         });
 
         const s3 = new AWS.S3();
