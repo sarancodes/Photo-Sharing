4e80330214ce4207524a6e0d566ad378b567acba
Saran Kumar <sarancodes@gmail.com>
2023-10-28 17:22:05 +0530

    Update awsconfig.js

--- src/awsconfig.js
+++ src/awsconfig.js
@@ -1,7 +1,7 @@
 export default {
-    accessKeyId: 'AKIATTU73NOEPC34P4LO',
-    secretAccessKey: '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',
-    region: 'us-east-1',
-    bucketName: 'myphotoaws',
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: '',
+    bucketName: '',
   };
   
